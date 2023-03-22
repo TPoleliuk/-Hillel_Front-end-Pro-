6317dcf87c5ca60b95376debdfc1a496d6b50e3e
unknown <poleluk@ukr.net>
2023-03-22 23:26:43 +0200

    Homework_3

---
+++ script.js
@@ -0,0 +1,27 @@
+const task1Part1 = document.getElementById('task1Part1');
+const task1Part2 = document.getElementById('task1Part2');
+const task2 = document.getElementById('task2');
+
+task1Part1.addEventListener('click', () => {
+    const userName = prompt("Please enter your name") || 'anonym';
+    alert(userName);
+})
+
+task1Part2.addEventListener('click', () => {
+    const userName = prompt("Please enter your name") === "admin";
+    alert (userName && "Hello admin" || 'Welcome')
+})
+
+task2.addEventListener('click', () => {
+    const login = prompt("Please enter your login");
+    const password = prompt("Please enter your password");
+
+    alert (login && password && 'welcome' || 'login or password incorrect');
+
+    login || password || alert('login and password incorrect')
+    login === "" && password && alert('login incorrect')
+    login && password === "" && alert('password incorrect')
+    
+    const age = prompt("Please enter your age");
+    alert (age && age >= 18 && age <= 28 && "Hello" || 'Goobay');
+})
