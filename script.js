2c5286399f40ca0ec966010ff66e96e13bc834ce
unknown <poleluk@ukr.net>
2023-04-04 00:10:28 +0300

    HW_13

---
+++ script.js
@@ -0,0 +1,18 @@
+const Arr = [1, 5, 5, 7, 9, 7, 3, 5, 1];
+
+console.log(Arr);
+
+function removeElement(array, item) {
+    for (let i = array.length - 1; i >= 0; i--) {
+        /*1. через цикл, бо якщо таких item декілька в масиві, то потрібно, щоб були видалені всі;
+          2. відлік у зворотньому напрямку, бо при видаленні елементу зміщуються індекси,
+          і якщо потрібні нам items стояли підряд, 2-й залишиться невидаленим*/
+
+        if (array[i] === item) {
+            array.splice(i, 1)
+        };
+    };
+    return array;
+};
+
+console.log(removeElement(Arr, 5));
