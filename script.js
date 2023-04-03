7a6a30184c4c7124e252e7b48db825eea6917aa1
unknown <poleluk@ukr.net>
2023-04-04 02:31:23 +0300

    HW_14

--- script.js
+++ script.js
@@ -2,36 +2,34 @@ const currentString = 'abcdefghijklmnopqdfsdfrstuvwxyz0123456789';
 console.log(currentString);
 
 function generateRandomNumber(minOfRange, maxOfRange) {
-    return Math.round(Math.random() * (maxOfRange - minOfRange + 1) + minOfRange - 0.5); //рандомно генеруємо число в заданому діапазоні;
+    return Math.round(Math.random() * (maxOfRange - minOfRange + 1) + minOfRange - 0.5); 
 };
 
 function generateKey(length, characters) {
-    let arrayFromCurrentString = characters.split('');  //перетворюємо посимвольно строку в масив;
-    let min = 0;                                        //мін. значення індексу в діапазоні, по якому здійснюватиметься пошук;
-    let max = arrayFromCurrentString.length - 1;        //макс. -//-;
-    let arrayForNewString = [];
+    let min = 0;                                        
+    let max = characters.length - 1;                    
+    let newString = '';
 
     for(let i = 0; i < length; i++) {
-        arrayForNewString[i] = arrayFromCurrentString[generateRandomNumber(min, max)]; //length разів рандомно генеруємо № індексу зі строки;
+        newString += characters[generateRandomNumber(min, max)]; 
     };
 
-    return arrayForNewString.join('');                   //формуємо з отриманого масиву строку;
+    return newString;                   
 };
 
-const key = generateKey(22, currentString);             //записуємо результат роботи функції в змінну key;
+const key = generateKey(22, currentString);            
 console.log(key);
 
 /*АБО якщо однією функцією:
-function generateKey(length, characters) {
-    let arrayFromCurrentString = characters.split('');  
+function generateKey(length, characters) { 
     let min = 0;                                        
-    let max = arrayFromCurrentString.length - 1;       
-    let arrayForNewString = [];
+    let max = characters.length - 1;       
+    let newString = '';
 
     for(let i = 0; i < length; i++) {
-        arrayForNewString[i] = arrayFromCurrentString[Math.round(Math.random() * (max - min + 1) + min - 0.5)];
+        newString += characters[Math.round(Math.random() * (max - min + 1) + min - 0.5)];
     };
 
-    return arrayForNewString.join('');                  
+    return newString;                 
 };
 */
