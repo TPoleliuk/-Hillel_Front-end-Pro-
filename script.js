23fb3cdd3cde2cbd88261ecccaaa347af3603034
unknown <poleluk@ukr.net>
2023-04-09 13:23:08 +0300

    Correct HW_17

--- script.js
+++ script.js
@@ -8,28 +8,26 @@ function isEven(x) {
 };
 
 function filter(input, checkingFunction) {
+    const newArray = [];
+    for (i = 0; i < input.length; i++) {
+        if (checkingFunction(input[i])) {
+            newArray.push(input[i]);                // АБО newArray[newArray.length] = input[i];
+        };
+    };
+    return newArray;
+};
+
+console.log(filter(array_1, isEven));
+
+/* АБО якщо НЕ створювати новий масив:
+function filter(input, checkingFunction) {
     for (let i = input.length - 1; i >= 0; i--) {
         if (!checkingFunction(input[i])) {
             input.splice(i, 1);
         };
     };
     return input;
-};
-
-/* АБО якщо створювати новий масив:
-
-function filter(input, checkingFunction) {
-    const newArray = [];
-    for (i = 0; i < input.length; i++) {
-        if (checkingFunction(input[i])) {
-            newArray[newArray.length] = input[i];
-        };
-    };
-    return newArray;
-};
-*/
-
-console.log(filter(array_1, isEven));
+}; */
 
 console.log('------------------------------------');
 
@@ -40,12 +38,40 @@ const array_2 = [7, 8, 9, 10, 11, 12];
 
 let multiplyBy10 = (value) => value * 10; 
 
-function copy(list, modifyingFunction) {
+function copy(list, modifyingFunction = (x) => x) {
     const newArray = [];
+
     for (let i = 0; i < list.length; i++) {
-        newArray[newArray.length] = modifyingFunction(list[i]);
+        newArray.push(modifyingFunction(list[i]));       // АБО newArray[newArray.length] = modifyingFunction(list[i]);
     };
     return newArray;
 };
 
-console.log(copy(array_2, multiplyBy10));
+console.log(copy(array_2));
+
+/* АБО через оператор ||:
+function copy(list, modifyingFunction) {
+    modifyingFunction = modifyingFunction || ((x) => x);
+    const newArray = [];
+
+    for (let i = 0; i < list.length; i++) {
+        newArray.push(modifyingFunction(list[i]));         // АБО newArray[newArray.length] = modifyingFunction(list[i]);
+    };
+    return newArray;
+}; */
+
+/* АБО через явну перевірку на underfined чи arguments.length:
+function copy(list, modifyingFunction) {
+    const newArray = [];
+
+    for (let i = 0; i < list.length; i++) {
+
+        if (modifyingFunction === undefined) {               // АБО if (arguments.length === 1);
+            newArray.push(list[i]);                          // АБО newArray[newArray.length] = list[i];
+        } else {
+            newArray.push(modifyingFunction(list[i]));       // АБО newArray[newArray.length] = modifyingFunction(list[i]);
+        };
+        
+    };
+    return newArray;
+}; */
