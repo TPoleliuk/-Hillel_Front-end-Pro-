7223c8cbc0ee3a8d1197f14cdc9388dbd1b7950e
unknown <poleluk@ukr.net>
2023-04-11 14:51:12 +0300

    Correct HW_18

--- script.js
+++ script.js
@@ -14,23 +14,24 @@ row(7);
 
 (console.log('-----------------------------------'))
 
-// Задача - 2. Написати рекурсивну функцію sumTo(n), яка для даного n обчислює суму чисел від 1 до n.
+// Задача - 2. Написати рекурсивну функцію sumToArray(n), яка обчислює суму вказаного масиву чисел.
 
-function sumTo(n) {
-    if (n === 1) {              //АБО if (!n) {return n}, але це дає ще один рекурсивний крок. 
-        return n;
+function sumToArray(arr) {
+    if (arr.length === 1) {     //АБО if (!arr.length) {return arr.length}, але це дає ще один рекурсивний крок (зайвий).
+        return arr[0];
     };
-    const sum = n + sumTo(n - 1);
+    const sum = arr[0] + sumToArray(arr.slice(1));
     return sum;
 };
-console.log(sumTo(6));
+
+console.log(sumToArray([1, 2, 3, 4, 5, 6]));
 
 (console.log('-----------------------------------'));
 
 // Задача - 3. Написати функцію factorial(n) - яка за допомогою рекурсії рахуватиме значення факторіалу числа n!
 
 function factorial(n) {
-    if (n === 1) {             //АБО if (!n) {return 1}, але це дає ще один рекурсивний крок. 
+    if (n === 1) {             //АБО if (!n) {return 1}, але це дає ще один рекурсивний крок (зайвий). 
         return n;
     };
     const product = n * factorial(n - 1);
