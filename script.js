204a39fb5ac0e5834fc352a566897ee10214cdb8
unknown <poleluk@ukr.net>
2023-04-18 21:12:53 +0300

    HW_23

--- script.js
+++ script.js
@@ -2,17 +2,14 @@ let ladder = {
     step: 0,
     up: function () {
         this.step++;
-        console.log(this)
         return this;
     },
     down: function () {
         this.step--;
-        console.log(this)
         return this;
     },
     showStep: function () { // показывает текущую ступеньку
         alert(this.step);
-        console.log(this)
         return this;
     },
 };
