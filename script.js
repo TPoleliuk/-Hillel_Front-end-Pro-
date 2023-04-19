1143b342aeb7872632ebb057e283e9f52ab56f5a
unknown <poleluk@ukr.net>
2023-04-19 14:08:35 +0300

    HW_24

--- script.js
+++ script.js
@@ -8,26 +8,34 @@ const obj = {
 };
 
 function renderObject() {
+    const breakRow = '<br>';
+    const colon = ':';
+    const space = ' ';
     for(key in this) {
         if (key === 'renderObject') continue;
-        document.write(key + ": " + this[key] + '<br>');
+        document.write(key + colon + space + this[key] + breakRow);
     };
+    document.write(breakRow);
 };
 
 obj.renderObject();
+renderObject.call(obj);
+renderObject.apply(obj);
 
 //Задача 2
 data = {
-    addRecord: function(){
-        for(i = 0; i < arguments.length; i++) {
-            for(key in arguments[i]) {
-                this[key] = arguments[i][key];
-            };
-        };
-    },
+    addRecord: addRecord,
     p: 600,
     str: 'hello',
-    y: -50
+    y: -50,
+};
+
+function addRecord() {
+    for(i = 0; i < arguments.length; i++) {
+        for(key in arguments[i]) {
+            this[key] = arguments[i][key];
+        };
+    };
 }
 
 data.addRecord({x: 10}, {y: 20}, {z: 30, x: 50});
@@ -35,5 +43,25 @@ data.addRecord({x: 10}, {y: 20}, {z: 30, x: 50});
 console.log(data.x) // 50
 console.log(data.y) // 20
 console.log(data.z) // 30
+console.log(data.p) // 600
+console.log(data.str) // 'hello'
+
+console.log('---------------------------')
+
+addRecord.call(data, {x: 110}, {y: 120}, {z: 130, x: 150});
+
+console.log(data.x) // 150
+console.log(data.y) // 120
+console.log(data.z) // 130
+console.log(data.p) // 600
+console.log(data.str) // 'hello'
+
+console.log('---------------------------')
+
+addRecord.apply(data, [{x: 210}, {y: 220}, {z: 230, x: 250}]);
+
+console.log(data.x) // 250
+console.log(data.y) // 220
+console.log(data.z) // 230
 console.log(data.p) // 600
 console.log(data.str) // 'hello'
