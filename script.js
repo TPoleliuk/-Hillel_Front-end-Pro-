b3d5a77ff23ec494751fe1d326ad7098bdaa81d8
unknown <poleluk@ukr.net>
2023-04-20 16:12:08 +0300

    HW_26

---
+++ script.js
@@ -0,0 +1,59 @@
+//Задача 2 (з ДЗ 24)
+const data = {
+    p: 600,
+    str: 'hello',
+    y: -50,
+};
+
+function addRecord() {
+    for(i = 0; i < arguments.length; i++) {
+        for(key in arguments[i]) {
+            this[key] = arguments[i][key];
+        };
+    };
+};
+
+/* АБО якщо потрібно трансформувати псевдомасив arguments в повноцінний масив:
+function addRecord() {
+    const arrFromArg = [].slice.call(arguments);   // const arrFromArg = [].slice.apply(arguments);
+    arrFromArg.forEach(arg => {
+        for(key in arg) {
+            this[key] = arg[key];
+        };
+    });
+};
+*/
+
+addRecord.call(data, {x: 110}, {y: 120}, {z: 130, x: 150});
+
+console.log(data.x) // 150
+console.log(data.y) // 120
+console.log(data.z) // 130
+console.log(data.p) // 600
+console.log(data.str) // 'hello'
+
+console.log('---------------------------')
+
+addRecord.apply(data, [{x: 210}, {y: 220}, {z: 230, x: 250}]);
+
+console.log(data.x) // 250
+console.log(data.y) // 220
+console.log(data.z) // 230
+console.log(data.p) // 600
+console.log(data.str) // 'hello'
+
+console.log('---------------------------')
+
+// АБО якщо потрібно записати аргументи в масив безпосередньо при виклику функції:
+
+function createArr() {
+    return [].slice.call(arguments);   // return [].slice.apply(arguments);
+};
+
+addRecord.apply(data, createArr({x: 210}, {y: 220}, {z: 230, x: 250}));
+
+console.log(data.x) // 250
+console.log(data.y) // 220
+console.log(data.z) // 230
+console.log(data.p) // 600
+console.log(data.str) // 'hello'
