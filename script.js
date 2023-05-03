d2e8ebff87434e7b6400ec6891c981991d5b77cb
unknown <poleluk@ukr.net>
2023-05-03 23:11:00 +0300

    HW_31

---
+++ script.js
@@ -0,0 +1,98 @@
+// fetch data from server
+const fetchAllProducts = async () => {
+  return (await fetch("https://dummyjson.com/products")).json();
+};
+
+function createImageDiv(image) {
+    return `
+        <div class="image-wrapper">
+            <img src="${image}" alt="image" class="image"/>
+        </div>
+    `;
+};
+
+function createTitleDiv(title) {
+    return `
+        <div class="title">
+            <h4>${title}</h4>
+        </div>
+    `;
+};
+
+function createPriceDiv(price, discount) {
+    const priceWithDiscount = getPriceWithDiscount(price, discount);
+
+    return `
+        <div class="price">
+            $${price}, price with discount $${priceWithDiscount}
+        </div>
+    `;
+};
+
+function getPriceWithDiscount(price, discount) {
+    const percent100 = 100;
+    const priceWithDiscountInCent = toCent(price) * (percent100 - discount) / percent100;
+    return toFloat(priceWithDiscountInCent).toFixed(2);
+};
+
+function toCent(price) {
+    const multiplier = 100;
+    return price * multiplier;
+};
+
+function toFloat(priceInCent) {
+    const divisor = 100;
+    return priceInCent / divisor;
+};
+
+function createDescriptionDiv(description) {
+    return `
+        <div class="description">${description}</div>
+    `;
+};
+
+function createActionsDiv() {
+    return `
+        <div class="actions">
+            <button id="cart"class="button green-solid cart">
+                Add to Cart
+            </button>
+            <button class="button more">
+                More Details
+            </button>
+        </div>
+    `;
+};
+
+function createProductItem(product) {
+    return `
+        <section class="product-item">
+            ${createImageDiv(product.thumbnail)}
+            <div class="content-wrapper">
+                ${createTitleDiv(product.title)}
+                ${createPriceDiv(product.price, product.discountPercentage)}
+                ${createDescriptionDiv(product.description)}
+                ${createActionsDiv()}
+            </div>
+        </section> 
+    `;
+};
+
+// Hometask --- refactoring  ---- decomposition
+async function getAllProducts() {
+    const response = await fetchAllProducts();
+    const prouducts = response.products;
+
+    const productsTamplate = `
+        <article class="products">
+            ${prouducts.map((product) => 
+                createProductItem(product)
+            ).join("")}
+        </article>
+    `;
+
+    document.getElementById("app").innerHTML = productsTamplate;
+};
+
+getAllProducts();
+
