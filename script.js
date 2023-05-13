b0ee80f6cf45d9d277b6a8db25081163fba8c914
unknown <poleluk@ukr.net>
2023-05-13 18:07:18 +0300

    HW_34

---
+++ script.js
@@ -0,0 +1,24 @@
+function* getPartsOfWord (string) {
+    let newString = '';
+
+    for (let i = 0; i < string.length; i++) {
+        let symbol = yield newString += string[i];
+
+        if (symbol === undefined) continue;
+        
+        newString += string[++i];
+        yield newString + symbol;
+    } 
+
+    return newString;
+};
+ 
+const iterator = getPartsOfWord("hello");
+
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next('!'));
+console.log(iterator.next());
+console.log(iterator.next('@'));
+console.log(iterator.next());
+
