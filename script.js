51e49f05bc2f13ce5bb1c6c08a898d58dae7ff8d
unknown <poleluk@ukr.net>
2023-05-13 18:38:57 +0300

    HW_34

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
+// function* getPartsOfWord (string) {
+//     let newString = '';
+
+//     for (let i = 0; i < string.length; i++) {
+//         let symbol = yield newString += string[i];
+
+//         if (symbol === undefined) continue;
+        
+//         newString += string[++i];
+//         yield newString + symbol;
+//     }; 
+
+//     return newString;
+// };
+
+// З 1 yield
+
 function* getPartsOfWord (string) {
     let newString = '';
+    let symbol = ''
 
     for (let i = 0; i < string.length; i++) {
-        let symbol = yield newString += string[i];
+        newString += string[i]
 
-        if (symbol === undefined) continue;
-        
-        newString += string[++i];
-        yield newString + symbol;
+        symbol = yield newString + symbol;
+
+        if (symbol === undefined) {
+            symbol = ''
+        };
     }; 
 
     return newString;
