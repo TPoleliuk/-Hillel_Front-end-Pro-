41f163a1819f5afaba6dd4feabe3494a8082d531
unknown <poleluk@ukr.net>
2023-06-06 14:03:12 +0300

    HW_39

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ class Person {
     }
 
     outputInfoPerson() {
-        console.log(this)
+        console.log(this);
     }
 };
   
@@ -24,24 +24,24 @@ class Car {
         if (person.age >= 18) {
             this.owner = person;
         } else {
-            console.log(`${person.name} is under 18 years old`)
-        }
+            console.log(`${person.name} is under 18 years old`);
+        };
     }
 
     outputInfoCarAndOwner() {
         console.log(this);
         if (this.owner) {
             this.owner.outputInfoPerson();
-        }
+        };
     }
 }
 
-const car1 = new Car('Toyota', 'Rav4', 2020, 'AA1111AA')
-const car2 = new Car('BMW', 'X6', 2022, 'AA2222AA')
+const car1 = new Car('Toyota', 'Rav4', 2020, 'AA1111AA');
+const car2 = new Car('BMW', 'X6', 2022, 'AA2222AA');
 console.log(car1, car2);
 
-car1.setOwner(person1)
-car1.setOwner(person2)
+car1.setOwner(person1);
+car1.setOwner(person2);
 console.log(car1, car2);
 
 car1.outputInfoCarAndOwner();
